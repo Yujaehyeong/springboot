5266e269cf82734cafff71b24d6d87155cedf28e
Yujaehyeong <mynameisyjh@gmail.com>
2019-06-05 19:53:37 +0900

    파일업로드 클래스 삭제및 pom.xml 디펜던시 수정~~

--- springboot-mysite/src/main/java/com/cafe24/mysite/config/WebConfig.java
+++ springboot-mysite/src/main/java/com/cafe24/mysite/config/WebConfig.java
@@ -8,18 +8,13 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.cafe24.config.web.FileUploadConfig;
 import com.cafe24.config.web.MVCConfig;
 import com.cafe24.config.web.MessageConfig;
 import com.cafe24.config.web.SecurityConfig;
-import com.cafe24.mysite.security.AuthInterceptor;
-import com.cafe24.mysite.security.AuthLoginInterceptor;
-import com.cafe24.mysite.security.AuthLogoutInterceptor;
-import com.cafe24.mysite.security.AuthUserHandlerMethodArgumentResolver;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({ "com.cafe24.mysite.controller" })
-@Import({ MVCConfig.class , SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
+@Import({ MVCConfig.class , SecurityConfig.class, MessageConfig.class})
 public class WebConfig extends WebMvcConfigurerAdapter {
 }
